11db20dd3825d635211993d02d0ef103efda9172
keating529 <mgsong@madup.com>
2021-02-16 13:57:05 +0900

    add reformat script

--- src/42583.test.js
+++ src/42583.test.js
@@ -1,30 +1,29 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42583
 
 function solution(bridge_length, weight, truck_weights) {
-  let time = 0;
-  const crossing = [];
-  const done = [];
-  const length = truck_weights.length;
+  let time = 0
+  const crossing = []
+  const done = []
+  const length = truck_weights.length
 
   while (done.length < length) {
-    time++;
+    time++
 
     if (crossing.length > 0 && time - crossing[0].time === bridge_length) {
-      done.push(crossing.shift());
+      done.push(crossing.shift())
     }
 
     const totalWeight = crossing.reduce(
       (acc, item) => acc + item.weight,
-      truck_weights[0]
-    );
+      truck_weights[0],
+    )
     if (totalWeight <= weight && crossing.length <= bridge_length) {
-      crossing.push({ time, weight: truck_weights.shift() });
+      crossing.push({ time, weight: truck_weights.shift() })
     }
   }
-  return time;
+  return time
 }
 
-
-test("test", () => {
-  expect(solution(2, 10, [7, 4, 5, 6])).toEqual(8);
+test('test', () => {
+  expect(solution(2, 10, [7, 4, 5, 6])).toEqual(8)
 })
