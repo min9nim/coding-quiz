b3a130ee4ad9c5e944876c26f3efce50667abc00
keating <mgsong@madup.com>
2021-11-23 09:30:39 +0900

    new swappable

---
+++ src/2021-11-swappable.test.js
@@ -0,0 +1,49 @@
+/*
+Q 두 개의 문자열 A, B가 주어졌을 때 A 문자열에서 두 개의 문자 위치를 교환해서 B가 되는지 판단하는 함수 구현
+e.g.,
+input : A = “abc”, B = “acb”
+output : true
+*/
+
+
+function swappable(a,b){
+  if(a.length !== b.length){
+    return false
+  }
+  if(a === b && new Set(a.split('')).size < a.length){
+    return true
+  }
+
+  let temp = []
+  let cnt = 0
+
+  for(let i = 0; i < a.length ; i++){
+    if(a[i] === b[i]){
+      continue
+    }else{
+      cnt++
+      if(cnt > 2){
+        return false
+      }
+      if(temp.length > 0){
+        if(!(temp[0] === b[i] && temp[1] === a[i])){
+          return false
+        }
+      }else{
+        temp = [ a[i], b[i] ]
+      }
+    }
+  }
+  return cnt >= 2
+}
+
+
+test('swapable', () => {
+  expect(swapable('aab', 'aab')).toEqual(true)
+  expect(swapable('abccc', 'acbbb') ).toEqual(false)
+  expect(swapable('abccc', 'acbbb')).toEqual(false)
+  expect(swapable('abccc', 'acbbb')).toEqual(false)
+  expect(swapable('abcwq', 'acbwq')).toEqual(true)
+  expect(swapable('abc', 'ayc')).toEqual(false)
+  expect(swapable('ab', 'ab')).toEqual(false)
+})
