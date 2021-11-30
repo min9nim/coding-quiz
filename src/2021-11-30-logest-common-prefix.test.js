68cface4a73b5ade7fca65a64d6802f4c4d090a6
keating <min9nim>
2021-11-30 13:21:39 +0900

    new

---
+++ src/2021-11-30-logest-common-prefix.test.js
@@ -0,0 +1,29 @@
+/*
+* https://leetcode.com/problems/longest-common-prefix/
+*/
+
+var longestCommonPrefix = function(strs) {
+  if(strs.length === 1){
+    return strs[0]
+  }
+  let prefix = ''
+  let i = 0
+  while(true){
+    for(let j=0; j<strs.length-1; j++){
+      if(!strs[j][i] || strs[j][i] !== strs[j+1][i]){
+        return prefix
+      }
+    }
+    prefix += strs[0][i]
+    i++
+  }
+};
+
+
+
+test('longestCommonPrefix', () => {
+  expect(longestCommonPrefix(["flower","flow","flight"])).toEqual('fl')
+  expect(longestCommonPrefix(["dog","racecar","car"])).toEqual('')
+  expect(longestCommonPrefix(["dog","racecar","car"])).toEqual('')
+  expect(longestCommonPrefix(["dog"])).toEqual('dog')
+})
